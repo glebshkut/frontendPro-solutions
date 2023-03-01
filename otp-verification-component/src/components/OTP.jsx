06c6ca40c564a918313d068bb2689e1880d6d480
Gleb <gleb.shkut@gmail.com>
2023-03-01 08:57:06 -0800

    added random codes

--- otp-verification-component/src/components/OTP.jsx
+++ otp-verification-component/src/components/OTP.jsx
@@ -1,9 +1,18 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 import Input from "./Input";
+
+const randomCode = (amount) => {
+  let code = "";
+  for (let i = 0; i < amount; i++) {
+    code += Math.floor(Math.random() * 10);
+  }
+  return code;
+};
 
 export default function OTP() {
   const inputsAmount = 4;
-  const answer = "1187";
+  const answer = useRef(randomCode(inputsAmount));
+
   const [active, setActive] = useState(0);
   const [otpValues, setOTPValues] = useState(new Array(inputsAmount).fill(""));
 
@@ -26,11 +35,17 @@ export default function OTP() {
 
   return (
     <div className="otp">
-      {answer.length === otpValues.length && otpValues.join("") === answer ? (
-        <div id="verified-text">Your email is verified!</div>
+      {answer.current.length === otpValues.length &&
+      otpValues.join("") === answer.current ? (
+        <div id="verified-div">
+          <div id="verified-text">Your email is verified!</div>
+          <button id="reload-button" onClick={() => window.location.reload()}>
+            Try again
+          </button>
+        </div>
       ) : (
         <div>
-          <p id="answer">Code: {answer}</p>
+          <p id="answer">Code: {answer.current}</p>
           <p className="main-text">Verify your email address</p>
           <div className="inputs">{inputsArray()}</div>
         </div>
